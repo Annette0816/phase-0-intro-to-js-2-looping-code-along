8036d69edd5be73f04c4aa4d45b2718a2eeb9c12
Annette <annette.ngugi@student.moringaschool.com>
2025-03-09 13:12:35 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solutions in this file
+// for (let age = 30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+//     debugger;
+//   }
+
+  
+  // const gifts = ["teddy bear", "drone", "doll"];
+
+  // function wrapGifts(gifts) {
+  //   for (let i = 0; i < gifts.length; i++) {
+  //     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  //     debugger;
+  //   }
+  
+  //   return gifts;
+  // }
+  
+  // wrapGifts(gifts);
+
+  const names = ["Charlie" , "Samip" , "Ali"];
+
+  
+  function writeCards (names){
+    let messages = [];
+     for (let i=0; i < names.length; i++) {
+       messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+        debugger;
+     }
+     return (messages);
+    }
+ 
+
+
+    function countDown(num) {
+        while (num>=0) {
+          console.log(num);
+            num--;
+        }
+    }
+ 
+
+
+
